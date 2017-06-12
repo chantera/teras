f6d64bdd9cffa72136fe4bc34c9612a8cd1d3fa5
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-06-13 02:31:35 +0900

    update root __init__.py

---
+++ teras/__init__.py
@@ -0,0 +1,55 @@
+"""
+
+================================================
+          ______
+         /_  __/__  _________ ______
+          / / / _ \/ ___/ __ `/ ___/
+         / / /  __/ /  / /_/ (__  )
+        /_/  \___/_/   \__,_/____/
+
+================================================
+
+Copyright 2017 Hiroki Teranishi
+
+@author  Hiroki Teranishi
+@email   teranishihiroki@gmail.com
+@url     https://github.com/chantera/teras/
+@licence MIT License
+
+------------------------------------------------------------------------------
+MIT License
+
+Copyright (c) 2017 Hiroki Teranishi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+------------------------------------------------------------------------------
+
+"""
+
+from . import app
+from . import base
+from . import dataset
+from . import framework
+from . import logging
+from . import preprocessing
+from . import training
+from . import utils
+
+__all__ = ['app', 'base', 'dataset', 'framework', 'logging', 'preprocessing',
+           'training', 'utils']
