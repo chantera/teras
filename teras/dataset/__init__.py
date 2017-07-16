3c980051dd1d3c5449443494e1086e24cdd20874
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-07-16 09:18:24 +0900

    reimplement Dataset.

--- teras/dataset/__init__.py
+++ teras/dataset/__init__.py
@@ -1,5 +1,4 @@
-from collections.abc import Iterable, Iterator, Sequence
-from operator import itemgetter
+from collections.abc import Sequence
 
 import numpy as np
 
@@ -12,55 +11,76 @@ class Dataset(Sequence):
     def __init__(self, *samples):
         self._n_cols = len(samples)
         if self._n_cols == 0:
-            self._samples = []
+            self._columns = ([],)
             self._n_cols = 1
-            self._dtype = list
         elif self._n_cols == 1:
-            self._dtype = type(samples[0])
-            if self._dtype is np.ndarray:
-                self._samples = samples[0]
-                self._n_cols = self._samples.shape[0]
+            if isinstance(samples[0], (list, tuple)):
+                columns = tuple(
+                    np.array(column)
+                    if isinstance(column[0], np.ndarray) else column
+                    for column in map(tuple, zip(*samples[0])))
+                self._columns = columns
+                self._n_cols = len(columns)
             else:
-                self._samples = list(samples[0])
-                first_sample = self._samples[0]
-                if (len(self._samples) > 0
-                        and (type(first_sample) is tuple
-                             or type(first_sample) is list)):
-                    self._n_cols = len(first_sample)
-                    self._dtype = (np.ndarray
-                                   if type(first_sample[0]) is np.ndarray
-                                   else type(first_sample))
+                self._columns = (samples[0],)
         elif self._n_cols > 1:
-            self._dtype = type(samples[0])
-            if self._dtype is np.ndarray:
-                self._samples = [_samples for _samples in zip(*samples)]
-            else:
-                self._samples \
-                    = [self._dtype(_samples) for _samples in zip(*samples)]
+            self._columns = samples
+        self._samples = list(map(tuple, zip(*self._columns)))
         self._len = len(self._samples)
-        self._indexes = np.arange(self._len)
+        self._indices = np.arange(self._len)
 
-    def batch(self, size, shuffle=False, colwise=False):
+    def __iter__(self):
+        return (sample for sample in self._samples)
+
+    def batch(self, size, shuffle=False, colwise=True):
         if shuffle:
-            np.random.shuffle(self._indexes)
-        return _DatasetBatchIterator(
-            Dataset(self.take(self._indexes)), size, colwise)
+            np.random.shuffle(self._indices)
+        if colwise:
+            return self._get_col_iterator(size)
+        else:
+            return self._get_row_iterator(size)
+
+    def _get_row_iterator(self, batch_size):
+        size = len(self)
+        offset = 0
+        while True:
+            if offset >= size:
+                raise StopIteration()
+            indices = self._indices[offset:offset + batch_size]
+            yield np.take(self._samples, indices, axis=0)
+            offset += batch_size
+
+    def _get_col_iterator(self, batch_size):
+        size = len(self)
+        offset = 0
+        while True:
+            if offset >= size:
+                raise StopIteration()
+            indices = self._indices[offset:offset + batch_size]
+            yield tuple(np.take(column, indices, axis=0)
+                        for column in self._columns)
+            offset += batch_size
 
     def __len__(self):
         return self._len
 
     def __getitem__(self, key):
-        if self._dtype == tuple:
-            return tuple(self._samples)[key]
-        return self._samples[key]
+        return self._samples.__getitem__(key)
 
     def take(self, indices):
-        if type(self._samples) is np.ndarray:
-            return self._samples.take(indices, axis=0)
-        elif isinstance(indices, Iterable):
-            _type = tuple if self._dtype is np.ndarray else self._dtype
-            return _type(itemgetter(*indices)(self._samples))
-        return self._samples[indices]
+        return np.take(self._samples, indices, axis=0)
+
+    def __reversed__(self):
+        return self._samples.__reversed__()
+
+    def __contains(self, sample):
+        return self._samples.__contains__(sample)
+
+    def index(self, sample):
+        return self._samples.index(sample)
+
+    def count(self, sample):
+        return self._samples.count(sample)
 
     def __repr__(self):
         return repr(self._samples)
@@ -69,18 +89,7 @@ class Dataset(Sequence):
         return str(self._samples)
 
     def cols(self):
-        if self._dtype is np.ndarray:
-            if self._n_cols > 1:
-                return np.swapaxes(self._samples, 0, 1)
-            return self._samples
-        else:
-            if self._n_cols > 1:
-                return tuple(self._dtype(col) for col in zip(*self._samples))
-            return self._dtype(self._samples),
-
-    @property
-    def dtype(self):
-        return self._dtype
+        return self._columns
 
     @property
     def size(self):
@@ -89,41 +98,3 @@ class Dataset(Sequence):
     @property
     def n_cols(self):
         return self._n_cols
-
-
-class _DatasetBatchIterator(Iterator):
-
-    def __init__(self, dataset, batch_size, colwise=False):
-        self._dataset = dataset
-        self._batch_size = batch_size
-        self._colwise = colwise
-
-    def __iter__(self):
-        dataset = self._dataset
-        container_type = type(dataset._samples)
-        dtype = dataset.dtype
-        if self._colwise:
-            if container_type is np.ndarray:
-                def _take(dataset, offset, batch_size):
-                    return dataset.cols()[:, offset:offset + batch_size]
-            elif dataset.n_cols == 1:
-                def _take(dataset, offset, batch_size):
-                    return dataset[offset:offset + batch_size],
-            else:
-                def _take(dataset, offset, batch_size):
-                    _type = np.array if dtype is np.ndarray else dtype
-                    return tuple(_type(col) for col in zip(
-                        *dataset._samples[offset:offset + batch_size]))
-        else:
-            def _take(dataset, offset, batch_size):
-                return dataset[offset:offset + batch_size]
-        size = dataset.size
-        offset = 0
-        while True:
-            if offset >= size:
-                raise StopIteration()
-            yield _take(dataset, offset, self._batch_size)
-            offset += self._batch_size
-
-    def __next__(self):
-        self.__iter__()
