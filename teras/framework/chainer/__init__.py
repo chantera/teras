524877efd204b6f76588e964a2a1783e5a60ea8c
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-08-27 14:42:44 +0900

    implement chainer optimizer hook ExponentialDecayAnnealing.

--- teras/framework/chainer/__init__.py
+++ teras/framework/chainer/__init__.py
@@ -1,11 +1,11 @@
 import chainer
 
-from teras.framework.chainer import callbacks, functions, model
+from teras.framework.chainer import callbacks, functions, model, optimizers
 from teras.training.trainer import TrainEvent
 
 __all__ = ['callbacks', 'chainer_train_on', 'chainer_train_off', 'config',
-           'functions', 'model', 'set_debug', 'set_model_to_device',
-           'to_device']
+           'functions', 'model', 'optimizers', 'set_debug',
+           'set_model_to_device', 'to_device']
 
 
 # hack
---
+++ teras/framework/chainer/optimizers.py
@@ -0,0 +1,20 @@
+class ExponentialDecayAnnealing(object):
+    name = 'ExponentialDecayAnnealing'
+    call_for_each_param = False
+
+    def __init__(self, decay_rate, decay_step, staircase=False, lr_key='lr'):
+        self.decay_rate = decay_rate
+        self.decay_step = decay_step
+        self.staircase = staircase
+        self.lr_key = lr_key
+        self.step = 0
+
+    def __call__(self, optimizer):
+        self.step += 1
+        lr = getattr(optimizer, self.lr_key)
+        print(lr)
+        p = self.step / self.decay_step
+        if self.staircase:
+            p //= 1  # floor
+        lr *= self.decay_rate ** p
+        setattr(optimizer, self.lr_key, lr)
