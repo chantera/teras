84bfa72371c82b77e389459087d103aa2a820374
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-09-05 06:05:17 +0900

    update.

--- teras/framework/chainer/optimizers.py
+++ teras/framework/chainer/optimizers.py
@@ -2,7 +2,9 @@ class ExponentialDecayAnnealing(object):
     name = 'ExponentialDecayAnnealing'
     call_for_each_param = False
 
-    def __init__(self, decay_rate, decay_step, staircase=False, lr_key='lr'):
+    def __init__(self, initial_lr, decay_rate, decay_step,
+                 staircase=False, lr_key='lr'):
+        self.initial_lr = initial_lr
         self.decay_rate = decay_rate
         self.decay_step = decay_step
         self.staircase = staircase
@@ -11,9 +13,8 @@ class ExponentialDecayAnnealing(object):
 
     def __call__(self, optimizer):
         self.step += 1
-        lr = getattr(optimizer, self.lr_key)
         p = self.step / self.decay_step
         if self.staircase:
             p //= 1  # floor
-        lr *= self.decay_rate ** p
+        lr = self.initial_lr * self.decay_rate ** p
         setattr(optimizer, self.lr_key, lr)
