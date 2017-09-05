5a87b54475e04639fc378e9aae0618af3ef50329
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-09-06 08:42:39 +0900

    preprocessing words with min frequency.

--- teras/preprocessing/text.py
+++ teras/preprocessing/text.py
@@ -1,6 +1,7 @@
-from collections import Iterable, UserDict
+from collections import defaultdict, Iterable, UserDict
 import copy
 import re
+import warnings
 
 import numpy as np
 
@@ -70,7 +71,7 @@ class Vocab(UserDict):
 
     @property
     def size(self):
-        return self.__len__()
+        return self._index + 1
 
     __marker = object()
 
@@ -106,6 +107,60 @@ class Vocab(UserDict):
         return default
 
 
+class _VocabWrapper(object):
+
+    def __init__(self, vocabulary, unknown):
+        self._vocabulary = vocabulary
+        self._unknown_id = self._vocabulary[unknown]
+
+    def fit(self, word):
+        self._vocabulary.add(word)
+        return self
+
+    def transform(self, word):
+        return self._vocabulary.get(word, self._unknown_id)
+
+    def fit_transform(self, word):
+        return self._vocabulary.add(word)
+
+    @property
+    def vocabulary(self):
+        return self._vocabulary
+
+    @property
+    def unknown_id(self):
+        return self._unknown_id
+
+
+class _FrequentVocabWrapper(_VocabWrapper):
+
+    def __init__(self, vocabulary, unknown, min_frequency=1):
+        super().__init__(vocabulary, unknown)
+        self._min_frequency = min_frequency
+        self._count = defaultdict(int)
+
+    def fit(self, word):
+        self._count[word] = count = self._count[word] + 1
+        if word not in self._vocabulary and count >= self.min_frequency:
+            self._vocabulary.add(word)
+        return self
+
+    def transform(self, word):
+        if self._count[word] < self._min_frequency:
+            return self._unknown_id
+        return self._vocabulary[word]
+
+    def fit_transform(self, word):
+        self._count[word] = count = self._count[word] + 1
+        if count < self.min_frequency:
+            return self._unknown_id
+        return self._vocabulary[word]
+
+    @property
+    def min_frequency(self):
+        return self._min_frequency
+
+
 def split(sentence):
     return sentence.split()
 
@@ -135,17 +190,21 @@ class Preprocessor(object):
                  pad=None,
                  tokenizer=split,
                  preprocess=lower,
+                 vocabulary=None,
+                 min_frequency=0,
                  index_dtype=np.int32):
-        if not hasattr(self, '_vocabulary'):
-            self._vocabulary = Vocab()
+        if vocabulary is None:
+            vocabulary = Vocab()
         if not preprocess:
             preprocess = raw
         self.index_dtype = index_dtype
-        self._pad_id = -1 if pad is None \
-            else self._add_vocabulary(preprocess(pad))
-        self._unknown_id = self._add_vocabulary(preprocess(unknown))
+        self._pad_id = vocabulary[pad] if pad is not None else -1
+        self._wrapper = \
+            _FrequentVocabWrapper(vocabulary, unknown, min_frequency) \
+            if min_frequency > 0 else _VocabWrapper(vocabulary, unknown)
         self._tokenizer = tokenizer
         self._preprocess = preprocess
+        self._min_frequency = min_frequency
 
     def set_tokenizer(self, tokenizer):
         self._tokenizer = tokenizer
@@ -153,62 +212,82 @@ class Preprocessor(object):
     def set_preprocess(self, func):
         self._preprocess = func
 
-    def _add_vocabulary(self, word):
-        return self._vocabulary[word]
-
-    def fit(self, raw_documents, as_one=False):
-        if isinstance(raw_documents, str) or as_one:
-            self._fit_each(raw_documents)
-        else:
-            for document in raw_documents:
-                self._fit_each(document)
+    def _fit_vocabulary(self, word):
+        self._wrapper.fit(word)
         return self
 
-    def _fit_each(self, raw_document):
-        for token in self._extract_tokens(raw_document):
-            self._add_vocabulary(token)
+    def _tranform_vocabulary(self, word):
+        return self._wrapper.transform(word)
 
-    def transform(self, raw_documents, length=None, as_one=False):
-        if isinstance(raw_documents, str) or as_one:
-            return self._transform_each(raw_documents, length)
+    def _fit_tranform_vocabulary(self, word):
+        return self._wrapper.fit_transform(word)
+
+    def fit(self, raw_documents):
+        if isinstance(raw_documents, str):
+            self.fit_one(raw_documents)
         else:
-            samples = [self._transform_each(document, length)
+            for document in raw_documents:
+                self.fit_one(document)
+        return self
+
+    def fit_one(self, raw_document):
+        for token in self._extract_tokens(raw_document):
+            self._fit_vocabulary(token)
+        return self
+
+    def transform(self, raw_documents, length=None):
+        if isinstance(raw_documents, str):
+            return self.transform_one(raw_documents, length)
+        else:
+            samples = [self.transform_one(document, length)
                        for document in raw_documents]
             return np.array(samples, self.index_dtype) if length else samples
 
-    def _transform_each(self, raw_document, length=None):
+    def transform_one(self, raw_document, length=None):
         tokens = self._extract_tokens(raw_document)
         if length is not None:
             word_ids = np.full(length, self._pad_id, dtype=self.index_dtype)
             for i, token in enumerate(tokens):
-                word_ids[i] = self.get_vocabulary_id(token)
+                word_ids[i] = self._tranform_vocabulary(token)
         else:
-            indices = [self.get_vocabulary_id(token) for token in tokens]
+            indices = [self._tranform_vocabulary(token) for token in tokens]
             word_ids = np.array(indices, self.index_dtype)
         return word_ids
 
-    def fit_transform(self, raw_documents, length=None, as_one=False):
-        if isinstance(raw_documents, str) or as_one:
-            ids = np.array([self._add_vocabulary(token)
-                            for token in self._extract_tokens(raw_documents)],
-                           self.index_dtype)
-            return _np_pad(ids, length, self._pad_id,
-                           self.index_dtype) if length else ids
-
+    def fit_transform(self, raw_documents, length=None):
+        if isinstance(raw_documents, str):
+            # fit and transform as one instance
+            return self.fit_transform_one(raw_documents, length)
+        elif self._min_frequency > 0:
+            # fit and then transform(
+            return self.fit(raw_documents).transform(raw_documents, length)
         elif length:
+            # fit and transform simultaneously (with padding)
             return np.vstack([
-                _np_pad(list(map(self._add_vocabulary,
+                _np_pad(list(map(self._wrapper.fit_transform,
                                  self._extract_tokens(raw_document))),
                         length,
                         self._pad_id,
                         self.index_dtype)
                 for raw_document in raw_documents])
         else:
+            # fit and transform simultaneously (without padding)
             return [
-                np.array(list(map(self._add_vocabulary,
+                np.array(list(map(self._wrapper.fit_transform,
                                   self._extract_tokens(raw_document))),
                          self.index_dtype)
                 for raw_document in raw_documents]
+
+    def fit_transform_one(self, raw_document, length=None):
+        if self._min_frequency > 0:
+            return self.fit_one(raw_document) \
+                .transform_one(raw_document, length)
+        else:
+            ids = np.array([self._wrapper.fit_transform(token)
+                            for token in self._extract_tokens(raw_document)],
+                           self.index_dtype)
+            return _np_pad(ids, length, self._pad_id,
+                           self.index_dtype) if length else ids
 
     def _extract_tokens(self, raw_document):
         if isinstance(raw_document, str):
@@ -232,23 +311,27 @@ class Preprocessor(object):
                 for array in tokens])
 
     def get_vocabulary_id(self, word):
-        return self._vocabulary.get(word, self._unknown_id)
+        return self._tranform_vocabulary(word)
 
     @property
     def vocabulary(self):
-        return self._vocabulary.copy()
+        return self._wrapper.vocabulary.copy()
 
     @property
     def vocabulary_size(self):
-        return self._vocabulary.size
+        return self._wrapper.vocabulary.size
 
     @property
     def unknown_id(self):
-        return self._unknown_id
+        return self._wrapper.unknown_id
 
     @property
     def pad_id(self):
         return self._pad_id
+
+    @property
+    def min_frequency(self):
+        return self._min_frequency
 
 
 def load_embeddings(embed_file, vocab_file=None, dtype=np.float32):
@@ -326,61 +409,56 @@ class EmbeddingPreprocessor(Preprocessor):
                  tokenizer=split,
                  initializer=standard_normal,
                  preprocess=lower,
+                 vocabulary=None,
+                 min_frequency=0,
                  embed_dtype=np.float32):
-        self.embed_dtype = embed_dtype
-        self._init_embeddings(embed_file, embed_size)
-        super(EmbeddingPreprocessor, self).__init__(
-            unknown, pad, tokenizer, preprocess, _get_int_type(embed_dtype))
-        self._initializer = initializer
-        if not self.use_pretrained and self._pad_id >= 0:
-            self.get_embeddings()[self._pad_id] = 0
-
-    def _init_embeddings(self, embed_file, embed_size):
         if embed_file is not None:
             vocab_file = None
             if isinstance(embed_file, (list, tuple)):
                 embed_file, vocab_file = embed_file
+            if vocabulary is not None:
+                warnings.warn("vocabulary will be overwritten "
+                              "by the dict of the embeddings")
             vocabulary, embeddings = \
-                load_embeddings(embed_file, vocab_file, self.embed_dtype)
+                load_embeddings(embed_file, vocab_file, embed_dtype)
             self.use_pretrained = True
             embed_size = embeddings.shape[1]
         elif embed_size is not None:
             if embed_size <= 0 or type(embed_size) is not int:
                 raise ValueError("embed_size must be a positive integer value")
-            vocabulary, embeddings = \
-                Vocab(), np.zeros((0, embed_size), self.embed_dtype)
+            if vocabulary is None:
+                vocabulary = Vocab()
+            embeddings = initializer(
+                (vocabulary.size, embed_size), embed_dtype)
             self.use_pretrained = False
         else:
             raise ValueError("embed_file os embed_size must be specified")
-
-        self._vocabulary = vocabulary
+        self.embed_dtype = embed_dtype
         self._embeddings = embeddings
-        self._new_words = []
         self._embed_size = embed_size
+        self._initializer = initializer
+
+        super(EmbeddingPreprocessor, self).__init__(
+            unknown, pad, tokenizer, preprocess,
+            vocabulary, min_frequency, _get_int_type(embed_dtype))
+        if not self.use_pretrained and self.pad_id >= 0:
+            self.get_embeddings()[self.pad_id] = 0
 
     def reset_embeddings(self, embed_size):
         self._embeddings = self._initializer(
-            (len(self._vocabulary), embed_size), self.embed_dtype)
+            (self.vocabulary_size, embed_size), self.embed_dtype)
         if self._pad_id >= 0:
             self._embeddings[self._pad_id] = 0
         self._embed_size = embed_size
         return self._embeddings
 
-    def _add_vocabulary(self, word):
-        if word not in self._vocabulary:
-            index = self._vocabulary[word]
-            self._new_words.append(index)
-        else:
-            index = self._vocabulary[word]
-        return index
-
     def get_embeddings(self):
-        uninitialized_vocab_size = len(self._new_words)
+        uninitialized_vocab_size = \
+            self.vocabulary_size - self._embeddings.shape[0]
         if uninitialized_vocab_size > 0:
             new_vectors = self._initializer(
                 (uninitialized_vocab_size, self._embed_size), self.embed_dtype)
             self._embeddings = np.r_[self._embeddings, new_vectors]
-            self._new_words = []
         return self._embeddings
 
     @property
@@ -389,7 +467,6 @@ class EmbeddingPreprocessor(Preprocessor):
 
     def __getstate__(self):
         state = self.__dict__.copy()
-        state['_new_words'] = []
         del state['_embeddings']
         return state
 
@@ -397,4 +474,4 @@ class EmbeddingPreprocessor(Preprocessor):
         self.__dict__.update(state)
         self.reset_embeddings()
         self._embeddings = np.zeros(
-            (self._vocabulary.size, self._embed_size), dtype=self.embed_dtype)
+            (self.vocabulary_size, self._embed_size), dtype=self.embed_dtype)
