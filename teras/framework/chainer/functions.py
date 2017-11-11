a2ea8d1d004c417381b176c94c424d7f7d44c601
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-11-11 15:01:19 +0900

    implement roll function.

--- teras/framework/chainer/functions.py
+++ teras/framework/chainer/functions.py
@@ -1,7 +1,10 @@
 from chainer import __version__ as chainer_version
 from chainer import as_variable
 from chainer import configuration
+from chainer import cuda
+from chainer import function_node
 import chainer.functions as F
+# from chainer.utils import type_check
 
 
 class EmbedIDFunction(
@@ -38,3 +41,34 @@ def dropout(x, ratio=.5):
     if configuration.config.train and ratio > .0:
         return F.noise.dropout.Dropout(ratio).apply((x,))[0]
     return as_variable(x)
+
+
+class Roll(function_node.FunctionNode):
+
+    """Roll array elements along a given axis."""
+
+    def __init__(self, shift, axis=None):
+        # @TODO: type check
+        self.shift = shift
+        self.axis = axis
+
+    def check_type_forward(self, in_types):
+        # @TODO: type check
+        pass
+
+    def forward(self, inputs):
+        self.retain_inputs(())
+        xp = cuda.get_array_module(*inputs)
+        return xp.roll(inputs[0], self.shift, self.axis),
+
+    def backward(self, indexes, gy):
+        if isinstance(self.shift, (list, tuple)):
+            shift = [-e for e in self.shift]
+        else:
+            shift = -self.shift
+
+        return Roll(shift, self.axis).apply(gy)
+
+
+def roll(x, shift, axis=None):
+    return Roll(shift, axis).apply((x,))[0]
